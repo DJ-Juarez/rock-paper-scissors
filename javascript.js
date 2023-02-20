c40c98754ef1e4161ebb7aecedaebb0cbaa8e4a9
DJ-Juarez <124060119+DJ-Juarez@users.noreply.github.com>
2023-02-20 14:42:50 +0100

    Add function that plays one round of the game
    User enter their choice in prompt and a message is printed in
    the console, declaring who won the round.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 
-
-let randomNumber = Math.floor(Math.random() * 3); // random number: 0, 1 or 2
-
-console.log(randomNumber);
+// function getComputerChoice generates a random number: 0, 1 or 2.
 
 function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3); 
     if (randomNumber === 0) {
         return "Rock";
     } else if (randomNumber === 1) {
@@ -16,4 +14,45 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+let playerSelection = prompt("Rock, Paper or Scissors?");
+let computerSelection = getComputerChoice();
+console.log (computerSelection);
+
+
+function singleRound() {
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") {
+            return "It's a tie!";
+        } else if (computerSelection === "Paper") {
+            return "You lose! Paper enclosa da Rock";
+        } else if (computerSelection === "Scissors") {
+            return "You win! Rock a smasha da Scissors";
+        } else {
+            return "Something went wrong.";
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Paper") {
+            return "It's a tie!";
+        } else if (computerSelection === "Scissors") {
+            return "You lose! Scissor clippa da Paper";
+        } else if (computerSelection === "Rock") {
+            return "You win! Paper enclosa da Rock";
+        } else {
+            return "Something went wrong.";
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Scissors") {
+            return "It's a tie!";
+        } else if (computerSelection === "Rock") {
+            return "You lose! Rock a smasha da Scissors";
+        } else if (computerSelection === "Paper") {
+            return "You win! Scissor clippa da Paper";
+        } else {
+            return "Something went wrong.";
+        }
+    } else {
+        return "Something went terribly wrong"
+    }  
+}
+
+console.log(singleRound());
