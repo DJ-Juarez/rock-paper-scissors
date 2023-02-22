494d210ba8cd19e78cc4450c231f252e17e0dfb5
DJ-Juarez <124060119+DJ-Juarez@users.noreply.github.com>
2023-02-22 14:27:42 +0100

    make the functions work together
    The program now runs as it should, and the code is improved overall.
    However the code could probably be condensed and written in a more
    clean way. The number of functions was decreased to 3 (previosly 4).

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
 
-// function getComputerChoice generates a random number: 0, 1 or 2, 
-// and returns one of three possible strings (Rock, Paper, Scissors).
+// getComputerChoice generates a random number: 0, 1 or 2.
+// It then returns one of three possible strings (Rock, Paper, Scissors).
 
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3); 
+    let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         return "Rock";
     } else if (randomNumber === 1) {
@@ -15,40 +15,34 @@ function getComputerChoice() {
     }
 }
 
-// function getPlayerChoice() asks user for a string (Rock, Paper, Scissors).
-// If input is invalid, the function restarts.
-// if run all the way correctly, it calls getComputerChoice() and outputs two strings.
 
-function getPlayerChoice() {
+// playRound() asks user for a string input (Rock, Paper, Scissors).
+// If input is invalid, the function restarts.
+// It calls getComputerChoice() and returns a string declaring who won the round.
+
+function playRound() {
 
     let playerSelection = prompt("Rock, Paper or Scissors?", "");
     let playerSelectionUpperCase;
     let playerSelectionLowerCase;
+    let computerSelection = getComputerChoice();
 
     if (playerSelection === null || "") {
         alert("Invalid choice. Restarting round.");
-        return getPlayerChoice();
+        return playRound();
     } else {
-    playerSelectionUpperCase = (playerSelection.charAt(0).toUpperCase());
-    playerSelectionLowerCase = (playerSelection.substring(1).toLowerCase());
-    playerSelection = playerSelectionUpperCase + playerSelectionLowerCase;
+        playerSelectionUpperCase = (playerSelection.charAt(0).toUpperCase());
+        playerSelectionLowerCase = (playerSelection.substring(1).toLowerCase());
+        playerSelection = playerSelectionUpperCase + playerSelectionLowerCase;
     }
-    if (playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors") { 
-        console.log ("You chose: ", playerSelection);
-        let computerSelection = getComputerChoice();
-        console.log ("Computer chose: ", computerSelection);  
-        return;      
+
+    if (playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors") {
+        console.log("You chose: ", playerSelection);
+        console.log("Computer chose: ", computerSelection);
     } else {
         alert("Check your spelling. Restarting round.");
-        return getPlayerChoice();
-    }  
-}
-
-
-function playRound() {
-    
-    getPlayerChoice(); 
-    // need to find a way to input playerSelection and computerSelection in this function
+        return playRound();
+    }
 
     if (playerSelection === "Rock") {
 
@@ -82,31 +76,42 @@ function playRound() {
             return "Something went wrong.";
         }
     } else {
-        return "Something went terribly wrong"
-    }  
-}
-
-// work in progress. Playround() is meant to loop 5 rounds of the game, and keep track of the score
-
-playRound();
-
-function game() {
-    alert("Let's play five rounds of Rock, Paper or Scissors.");
-
-    let getRoundResult = playRound();
-    let x = 0;
-    let y = 0;
-
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        if (getRoundResult.includes("win")) {
-            x += 1;      // add point to player
-       } else if (getRoundResult.includes("lose")) {
-            y += 1;      // add point to computer
-       } 
-       console.log(playRound());
-       console.log(`Score: Player - ${x}, Computer ${y}`);
+        return "Something went terribly wrong";
     }
 }
 
 
+// game() loops playRound() 5 times, and outputs score to the console.
+
+function game() {
+    alert("Let's play five rounds of Rock, Paper or Scissors.");
+
+    let x = 0; // Player's points
+    let y = 0; // Computer's points
+
+    for (let i = 0; i < 5; i++) {
+        console.log("ROUND " + (i + 1) + ":")
+        let getRoundResult = playRound();
+        console.log(getRoundResult);
+
+        if (getRoundResult.includes("win")) {
+            x += 1;      // add point to player
+        } else if (getRoundResult.includes("lose")) {
+            y += 1;      // add point to computer
+        }
+        console.log(`SCORE: Player - ${x}, Computer - ${y}`);
+        console.log("");
+
+        if (i < 4) {
+            //alert("Next round!");
+        } else if (x > y) {
+            console.log("You win! Refresh browser to play again.");
+        } else if (x < y) {
+            console.log("You lose! Refresh browser to play again.");
+        } else if (x == y) {
+            console.log("It's a draw! Refresh browser to play again.");
+        }
+    }
+}
+
+game();
